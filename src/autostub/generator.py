c8f4b2576b0c6bc2b6832a576e3e46f2f9efbc9e
Pavel Zhukov <zhukoff.pavel@yandex.ru>
2024-05-29 00:16:02 +0300

    First commit

---
+++ src/autostub/generator.py
@@ -0,0 +1,8 @@
+class BaseGenerator:
+    def __init__(self) -> None:
+        pass
+
+
+class OAPISpecGenerator(BaseGenerator):
+    def __init__(self) -> None:
+        super(OAPISpecGenerator).__init__()
